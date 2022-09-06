cd76a90e403207b39f11f698f1cc5b633b95ecfd
Student astrum <student@astrum.domain>
2022-09-06 20:32:06 +0500

    my-yelp

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "myyelp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { createRestaurant, deleteRestaurant} from './graphql/mutations'
+import { listRestaurants } from './graphql/queries'
+import { withAuthenticator, Button, Text, Flex, Heading } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
 
-function App() {
+function App({ signOut }) {
+  const [ notes, setNotes ] = useState([])
+
+  const fetchNotes = useCallback(async () => {
+    const result = await API.graphql({
+      query: listRestaurants,
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    setNotes(result.data.listRestaurants.items)
+  }, [setNotes])
+
+  const handlecreateRestaurant = useCallback(async () => {
+    await API.graphql({
+      query: createRestaurant,
+      variables: { input: { text: window.prompt("New restourant") } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  const handledeleteRestaurant = useCallback(async (id) => {
+    await API.graphql({
+      query: deleteRestaurant,
+      variables: { input: { id: id } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  useEffect(() => {
+    fetchNotes()
+  }, [fetchNotes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction={"column"}>
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My notes</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      {notes.map(note => <Flex alignItems={'center'}>
+        <Text>{note.text}</Text>
+        <Button onClick={() => handledeleteRestaurant(note.id)}>Remove</Button>
+      </Flex>)}
+      <Button onClick={handlecreateRestaurant}>Add Note</Button>
+    </Flex>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
